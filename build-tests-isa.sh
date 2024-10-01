507d4e3e31c7885509f6984ec1d5e9ae4b902a41
Philippe Anel <philippe.anel@mediawen.com>
2024-10-01 07:29:15 +0200

    fixed compilation errors

--- build-tests-isa.sh
+++ build-tests-isa.sh
@@ -9,6 +9,13 @@ for arg in "$@"; do
 done
 
 
-for a in tests/isa/*.asm; do b=`echo $a | sed 's/\.asm/.bin/'`; nasm -f bin -DDEBUG -Itests -o$b $a ; done
+for a in tests/isa/*.asm; do
+    b=`echo $a | sed 's/\.asm/.bin/'`
+    nasm -f bin -DDEBUG -Itests -o$b $a
+    if [ ! -f "$b" ]; then
+        echo "Error: Failed to compile $a. Output file $b does not exist."
+        exit 1
+    fi
+done
 
 cargo r --bin emu8086 -- -test -hide-header $log -show-binary-name -dump-regs-on-halt tests/isa/*.bin
--- tests/isa/sbb.asm
+++ tests/isa/sbb.asm
@@ -1,6 +1,7 @@
 CPU     8086
 BITS    16
 ORG     0       ; !!! hey: in fact, we start at 0xf000:0x0000
+%include "expect.inc"
 
 _start:
         MOV AL, 0x05
